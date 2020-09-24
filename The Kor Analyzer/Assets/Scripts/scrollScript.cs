b93f351a1594b24d6756f6b9546cd0e7144ef36e
cseischmidt <ischmidt@citystateentertainment>
2020-09-23 20:10:24 -0400

    more scrolling lists stuff
    found some guides on how to do a scrolling list of buttons, still very much in process and not at all useful yet

---
+++ The Kor Analyzer/Assets/Scripts/scrollScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scrollScript : MonoBehaviour
+{
+	public ScrollRect scrollView;
+	public GameObject scrollContent;
+	public GameObject scrollItemPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int a = 1; a <= 20; a++)
+        {
+        	generateItem(a);
+        }
+        scrollView.verticalNormalizedPosition = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void generateItem(int itemNumber)
+    {
+    	GameObject scrollItemObj = Instantiate(scrollItemPrefab);
+    	scrollItemObj.transform.SetParent(scrollContent.transform, false);
+    	scrollItemObj.transform.Find("num").gameObject.GetComponent<Text>().text = itemNumber.ToString();
+
+    }
+}
